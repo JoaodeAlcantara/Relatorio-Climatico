fc151e7333217285caed2dbe90079d836f3f3d63
JoaodeAlcantara <joao.gab0509@gmail.com>
2024-11-03 14:09:08 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,70 @@
+import { CreactTable, getCidade, getEstado, innerHTML } from "./script.js"
+
+const date = new Date()
+const dateFormat = (date).toLocaleDateString("pt-BR")
+document.querySelector('input').value = dateFormat
+const hrAtual = (date).toLocaleTimeString('pt-br')
+document.querySelector('#dt-hr').innerHTML = dateFormat + ' às ' + hrAtual.slice(0, 5)
+
+window.addEventListener('load', () => {
+    getEstado()
+})
+
+document.querySelector('#estado').addEventListener('change', function () {
+    const estado = this.value
+    document.querySelector('#estadoTable').innerHTML = ' - ' + estado;
+    getCidade(estado)
+})
+
+document.querySelector('#cidade').addEventListener('change', async function () {
+    const cidade = this.value
+    const apiKey = `d5a9f2d2a7a9469d95e140826240211`;
+
+    try {
+        const resp = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${cidade}&days=1&aqi=yes&alerts=yes&lang=pt`)
+
+        if(!resp.ok){
+            throw new Error(`Erro: ${resp.status} - ${resp.statusText}`)
+        }
+        const dados = await resp.json()
+
+        erro.classList.add('hyde');
+
+        const air_quality = await dados.current.air_quality;
+        const { co, no2, o3, pm2_5, pm10, so2 } = air_quality;
+        const gbDefraIndex = air_quality["gb-defra-index"];
+        const table = document.querySelector('table');
+
+        innerHTML(co, no2, o3, pm2_5, pm10, so2, gbDefraIndex);
+
+        table.innerHTML = ''
+        table.innerHTML = `<tr>
+                    <th>Horário</th>
+                    <th>Condição</th>
+                    <th>Chuva (%)</th>
+                    <th>Temperatura (C°)</th>
+                    <th>Sensasão (C°)</th>
+                    <th>Umidade (%)</th>
+                    <th>Vento (Kph)</th>
+                </tr>`
+
+        dados.forecast.forecastday[0].hour.map(i => {
+            const info = new CreactTable(i.time.slice(11, 16),
+                i.condition.text, i.chance_of_rain, i.temp_c, i.heatindex_c,
+                i.humidity, i.wind_kph);
+            
+            table.innerHTML += info.insertInto();
+        })
+        document.querySelector('#cidadeTable').innerHTML = dados.location.name
+        document.querySelector('.table').classList.remove('hyde')
+
+    } catch (error) {
+        const erro = document.querySelector('#msgErro');
+        console.error('Erro ao buscar: ', error.message)
+        document.querySelector('#erro').classList.remove('hyde');
+        document.querySelector('.table').classList.add('hyde')
+        erro.innerHTML = 'Erro ao buscar', error.statusText
+    }
+
+})
+
---
+++ js/script.js
@@ -0,0 +1,78 @@
+export class CreactTable {
+    constructor(hr, cond, chuva, temp, sens, umi, vento) {
+        this.hr = hr
+        this.condicao = cond
+        this.chuva = chuva
+        this.temperatura = temp
+        this.sensacao = sens
+        this.umidade = umi
+        this.vento = vento
+    }
+
+    insertInto() {
+        const line =
+            `<tr class='lineInfo'>
+            <td>${this.hr}</td>
+            <td>${this.condicao}</td>
+            <td>${this.chuva}</td>
+            <td>${this.temperatura}</td>
+            <td>${this.sensacao}</td>
+            <td>${this.umidade}</td>
+            <td>${this.vento}</td>
+        </tr>`
+        return line
+    }
+}
+
+export async function getEstado() {
+    const resp = await fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados');
+    const dados = await resp.json()
+    const selectEstado = document.querySelector('#estado');
+
+    console.log(dados)
+
+    dados.map(i => {
+        selectEstado.innerHTML += `<option value="${i.sigla}" id='opE'>${i.nome}</option>`
+    })
+}
+
+export async function getCidade(estado) {
+    const selectCidade = document.querySelector('#cidade');
+    selectCidade.innerHTML = ''
+    selectCidade.innerHTML += `<option value="#">Selecione uma cidade</option>`;
+    const resp = await fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${estado}/municipios`)
+    const dados = await resp.json()
+
+    dados.map(i => {
+        selectCidade.innerHTML += `<option value="${i.nome}">${i.nome}</option>`;
+    })
+
+}
+
+export function innerHTML(co, no2, o3, pm2_5, pm10, so2, gbDefraIndex) {
+    const qAr = document.querySelector('#qualidadeAr');
+
+    if (gbDefraIndex >= 1 && gbDefraIndex <= 3) {
+        qAr.innerHTML = 'Baixa'
+        qAr.style.backgroundColor = 'green';
+    } else if (gbDefraIndex > 3 && gbDefraIndex <= 6) {
+        qAr.innerHTML = 'Moderada'
+        qAr.style.backgroundColor = 'orange';
+    } else if (gbDefraIndex > 6 && gbDefraIndex <= 9) {
+        qAr.innerHTML = 'Alta'
+        qAr.style.backgroundColor = 'orangered';
+    } else {
+        qAr.innerHTML = 'Muito Alta'
+        qAr.style.backgroundColor = 'red';
+    }
+
+    const info = document.querySelectorAll('.dados');
+    const [infoCo, infoNo2, infoO3, infoPm2_5, infoPm10, infoSo2] = info;
+    infoCo.innerHTML = '<i>co: </i>' + co;
+    infoNo2.innerHTML = '<i>no2: </i>' + no2;
+    infoO3.innerHTML = '<i>o3: </i>' + o3;
+    infoPm2_5.innerHTML = '<i>pm2.5: </i>' + pm2_5;
+    infoPm10.innerHTML = '<i>pm10: </i>' + pm10;
+    infoSo2.innerHTML = '<i>so2: </i>' + so2;
+}
+
